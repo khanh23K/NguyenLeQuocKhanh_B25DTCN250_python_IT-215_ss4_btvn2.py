1cc9ced2a69935789b70ca7994ca321233f1ad4c
khanh23K <khanhkinh23@gmail.com>
2026-06-29 22:26:46 +0700

    Upload btvn2.py via Git Upload 27NetTeam

---
+++ btvn2.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+app = FastAPI()
+orders = [
+    {"id": 1, "customer_name": "Nguyễn Văn An", "total": 250000, "status": "pending"},
+    {"id": 2, "customer_name": "Trần Thị Bình", "total": 500000, "status": "paid"},
+    {"id": 3, "customer_name": "Lê Văn Cường", "total": 150000, "status": "cancelled"},
+    {"id": 4, "customer_name": "Phạm Thị Dung", "total": 320000, "status": "pending"}
+]
+@app.get("/orders/status/{status}")
+def get_orders_by_status(status: str):
+    value_status = ["pending","paid","cancelled"]
+    if status not in value_status:
+        return {"message": "trạng thái ko hợp lệ!!!!"}
+
+    result = []
+    for order in orders:
+        if order["status"] == status:
+            result.append(order)
+    return result
